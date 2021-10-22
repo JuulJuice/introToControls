6f0ba02f515758511e2a882f9b5adc95fe69e034
Julian Jackson <taz38877@msn.com>
2021-10-22 09:35:50 -0400

    yes

--- introToControls/ViewController.swift
+++ introToControls/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func settext(_ sender: Any) {
+        label.text = textfield.text
+    }
+    
+    
+    @IBAction func cleartext(_ sender: Any) {
+        label.text = ""
+    }
+    
+    @IBOutlet weak var textfield: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
